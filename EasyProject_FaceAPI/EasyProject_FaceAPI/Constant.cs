011f6e71e70ee4ff966da52ac780ddc6b44a5530
yinnping <ciaoing@livemail.tw>
2017-01-02 20:01:45 +0800

    Upadte

--- EasyProject_FaceAPI/EasyProject_FaceAPI/Constant.cs
+++ EasyProject_FaceAPI/EasyProject_FaceAPI/Constant.cs
@@ -5,8 +5,8 @@ namespace EasyProject_FaceAPI
     class Constant
     {
         // TODO: Prepare. 事前準備
-        public static string FaceKey = "Your Key";
-        // public static Uri ImageUri = new Uri("http://i.imgur.com/lfP6evD.jpg");
-        public static Uri ImageUri = new Uri("https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/10847765_987795661235969_5085961470711725489_o.jpg?oh=e8b2619b740083ad50b0d511c6b4df0c&oe=58DC134D");
+        public static string FaceKey = "7a9292d350c44097a23b20fd303d5573";
+        public static Uri ImageUri = new Uri("http://i.imgur.com/lfP6evD.jpg");
+        // public static Uri ImageUri = new Uri("https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/10847765_987795661235969_5085961470711725489_o.jpg?oh=e8b2619b740083ad50b0d511c6b4df0c&oe=58DC134D");
     }
 }
--- EasyProject_FaceAPI/EasyProject_FaceAPI/FaceFunction.cs
+++ EasyProject_FaceAPI/EasyProject_FaceAPI/FaceFunction.cs
@@ -1,28 +1,35 @@
-﻿namespace EasyProject_FaceAPI
+﻿using DrawFunction;
+using Microsoft.ProjectOxford.Face;
+using System.Threading.Tasks;
+
+namespace EasyProject_FaceAPI
 {
     class FaceFunction
     {
         // TODO: 1. 利用 FaceServiceClient 連線至 Cognitive service，並建立相關方法
-            // Step 1: new FaceServiceClient("Your key")
-        
-        //public async Task<MyDataType> DectedFace()
-        //{
-        //    // 設定 FaceAttributeType 屬性的陣列，可以選擇要回傳哪些屬性 
-        //    FaceAttributeType[] fats = new FaceAttributeType[]
-        //    {
-        //        FaceAttributeType.Age
-        //    };
+        // Step 1: new FaceServiceClient("Your key")
 
-        //    // TODO: 2. 使用 FaceServiceClient 提供的 DetectAsync 方法取得已辨識的臉部資訊
-        //        // Step 1: FaceServiceClient.DetectAsync(string, ...)                 
+        public async Task<MyDataType> DectedFace()
+        {
+            // 設定 FaceAttributeType 屬性的陣列，可以選擇要回傳哪些屬性 
+            FaceAttributeType[] fats = new FaceAttributeType[]
+            {
+                FaceAttributeType.Age
+            };
 
-        //    // TODO: 3. 建立 MyDataType 型別的物件，整理 FaceServiceClient 傳回的資訊，方便後續使用
-        //        // Step 1: new MyDataType();
-        //        // Step 2: foreach (faces)
-        //        // Step 3: rects.Add(new Rect(int, int, int))
-        //        // Step 4: ages.Add(new Age(FaceAttributes.Age))            
-            
-        //    return new MyDataType();
-        //}
+            // TODO: 2. 使用 FaceServiceClient 提供的 DetectAsync 方法取得已辨識的臉部資訊
+            // Step 1: FaceServiceClient.DetectAsync(string, ...)      
+            FaceServiceClient fsc = new FaceServiceClient(Constant.FaceKey);
+            var faces = fsc.DetectAsync(Constant.ImageUri.ToString(), true, false, fats);        
+
+            // TODO: 3. 建立 MyDataType 型別的物件，整理 FaceServiceClient 傳回的資訊，方便後續使用
+            // Step 1: new MyDataType();
+            // Step 2: foreach (faces)
+            // Step 3: rects.Add(new Rect(int, int, int))
+            // Step 4: ages.Add(new Age(FaceAttributes.Age))         
+               
+
+            return new MyDataType();
+        }
     }
 }
